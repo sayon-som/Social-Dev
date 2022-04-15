c9309c1d5c4872620bd78461996ef743172a095e
Sayon Som <sayonsom11b@gmail.com>
2022-04-15 18:07:49 +0530

    model and body validation created

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,30 @@
-const express=require('express');
-const Router=express.Router();
+const express = require("express");
+const Router = express.Router();
+//for express validation
+const { body, validationResult, check } = require("express-validator");
 
-// @route  GET api/user
-// @desc   Test Route
+// @route  POST api/user
+// @desc   Registering the user
 // @access Public
 
-Router.get("/",(req,res)=>{
-    res.status(200).json({msg:"you are using the user route"})
-    
-})
+Router.post("/", 
+[
+    check('name',"Name is needed").not().notEmpty(),
+    check('email',"Email not valid").isEmail(),
+    check('password','password should be more than 7 characters').isLength({min:7})
+]
+,(req, res) => {
+//   res.status(200).json({ msg: req.body });
+const errors=validationResult(req);
+//in case of errors
+if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()});
 
-module.exports=Router;
+}
+//errors are missing 
+else{
+    res.json({msg:"worked fine"});
+}
+});
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const connect=require('./config/db');
 //getting the routers
 const UserRouter=require('./routes/api/user');
 const AuthRouter = require("./routes/api/auth");
-const PostRouter = require("./routes/api/user");
-const Router = require("./routes/api/user");
+const PostRouter = require("./routes/api/post");
+const ProfileRouter = require("./routes/api/profile");
+
+//for using the body parser functionalities
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 //testing
 app.get("/",(req,res)=>{
     res.status(200).json({msg:"API is running"})
@@ -17,7 +22,10 @@ connect();
 
 
 //adding the routes functionalitites
-app.use("/user",UserRouter);
+app.use("/api/user",UserRouter);
+app.use("/api/post",PostRouter);
+app.use("/api/auth",AuthRouter);
+app.use("/api/profile",ProfileRouter);
 
 const PORT=process.env.PORT || 8000;
 app.listen(PORT,()=>{
